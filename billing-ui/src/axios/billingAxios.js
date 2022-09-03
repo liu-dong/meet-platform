57418bfd85247b21b73fedc7bc68844964384c8d
liudong <964708273@qq.com>
2022-09-04 00:10:38 +0800

    2.0版本整理

--- billing-ui/src/axios/billingAxios.js
+++ billing-ui/src/axios/billingAxios.js
@@ -1,5 +1,6 @@
 import builder from "@/axios";
 
-const request = builder({ baseURL: process.env.VUE_APP_BILLING_API })
+// const request = builder({ baseURL: process.env.VUE_APP_BILLING_API })
+const request = builder({ baseURL: '/apiBilling' })
 
 export default request
--- billing-ui/vue.config.js
+++ billing-ui/vue.config.js
@@ -1,13 +1,18 @@
 module.exports = {
     // 修改的配置
     devServer: {
-        port: 8081
-        /*proxy: {
-            '/api': {
+        port: 8081,
+        proxy: {
+            '/apiAuth': {
+                target: 'http://localhost:8180',
+                changeOrigin: true,
+                pathRewrite: {'^/apiAuth': ''}
+            },
+            '/apiBilling': {
                 target: 'http://localhost:8181',
                 changeOrigin: true,
-                pathRewrite: {'^/api': ''}
+                pathRewrite: {'^/apiBilling': ''}
             }
-        }*/
+        }
     }
 }
