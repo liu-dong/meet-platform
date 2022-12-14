2afb05974559e8d838e4a2fda28f64a4e341f9c7
liudong <liudong@zsoft.com.cn>
2022-12-14 15:29:26 +0800

    【common-core】 修改内容：添加统一返回数据格式

--- common-core/src/main/java/com/dong/commoncore/advice/ResponseResultAdvice.java
+++ common-core/src/main/java/com/dong/commoncore/advice/ResponseResultAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@RestControllerAdvice(basePackages = "com.dong")
+@RestControllerAdvice(basePackages = "com.dong.**")
 public class ResponseResultAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
