00f40642d1c2e4829a4235c23c543adbbe7a2e46
liudong <liudong@zsoft.com.cn>
2023-02-06 10:25:42 +0800

    commit

--- admin-server/src/main/java/com/dong/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/dong/adminserver/AdminServerApplication.java
@@ -2,8 +2,6 @@ package com.dong.adminserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class AdminServerApplication {
