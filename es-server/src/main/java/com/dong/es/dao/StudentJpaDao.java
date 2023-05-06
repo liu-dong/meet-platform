7a99791bb79a08806accf58e1093c9bf670ee974
liudong <liudong@zsoft.com.cn>
2023-05-06 18:04:04 +0800

    commit

---
+++ es-server/src/main/java/com/dong/es/dao/StudentJpaDao.java
@@ -0,0 +1,14 @@
+package com.dong.es.dao;
+
+import com.dong.demo.es.domain.Student;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author liudong
+ * @date 2023/5/6
+ */
+@Repository
+public interface StudentJpaDao extends ElasticsearchRepository<Student, String> {
+
+}
