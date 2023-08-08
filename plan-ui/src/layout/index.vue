c9418729751cf987021b98f65a5ef1045d21e009
liudong <964708273@qq.com>
2023-08-08 23:30:25 +0800

    commit

--- plan-ui/src/layout/index.vue
+++ plan-ui/src/layout/index.vue
@@ -1,26 +1,20 @@
 <template>
-  <div :class="classObj" class="app-wrapper">
-    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"/>
+  <div class="app-wrapper">
     <navigationbar/>
-    <!--<sidebar class="sidebar-container" />-->
     <div class="main-container">
-      <!--<div :class="{'fixed-header':fixedHeader}">-->
-      <!--  <navbar/>-->
-      <!--</div>-->
       <app-main/>
     </div>
   </div>
 </template>
 
 <script>
-import {AppMain, Navbar, Navigationbar} from './components'
+import {AppMain, Navigationbar} from './components'
 import ResizeMixin from './mixin/ResizeHandler'
 
 export default {
   name: 'Layout',
   components: {
     Navigationbar,
-    Navbar,
     AppMain
   },
   mixins: [ResizeMixin],
@@ -57,40 +51,12 @@ export default {
 
 .app-wrapper {
   @include clearfix;
+  border: 5px solid red;
   position: relative;
+  //top: 0;
+  //left: 0;
   height: 100%;
   width: 100%;
-
-  &.mobile.openSidebar {
-    position: fixed;
-    top: 0;
-  }
 }
 
-.drawer-bg {
-  background: #000;
-  opacity: 0.3;
-  width: 100%;
-  top: 0;
-  height: 100%;
-  position: absolute;
-  z-index: 999;
-}
-
-.fixed-header {
-  position: fixed;
-  top: 0;
-  right: 0;
-  z-index: 9;
-  width: calc(100% - #{$sideBarWidth});
-  transition: width 0.28s;
-}
-
-.hideSidebar .fixed-header {
-  width: calc(100% - 54px)
-}
-
-.mobile .fixed-header {
-  width: 100%;
-}
 </style>
