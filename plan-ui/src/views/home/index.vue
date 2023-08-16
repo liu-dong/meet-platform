b3db601743f171a9a3b3e8958c2984adb214bcac
liudong <964708273@qq.com>
2023-08-16 22:38:42 +0800

    commit

---
+++ plan-ui/src/views/home/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="dashboard-container">
+    <h1>日历</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Home',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
