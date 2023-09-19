82657069c390c97a19454d037d6a2a9022c0eec8
liudong <liudong@zsoft.com.cn>
2023-09-19 17:48:41 +0800

    commit

--- admin-web/src/router/adminRouter.js
+++ admin-web/src/router/adminRouter.js
@@ -29,64 +29,97 @@ export const adminRouter = [
     ]
   },
   {
-    path: '/accountList',
-    name: 'accountList',
-    meta: { title: '账号列表', icon: 'table' },
-    component: () => import('@/views/admin/AccountList.vue')
+    path: '/account',
+    name: 'Account',
+    redirect: '/accountList',
+    component: Layout,
+    children: [
+      {
+        path: '/accountList',
+        name: 'accountList',
+        meta: { title: '账号列表', icon: 'table' },
+        component: () => import('@/views/admin/AccountList.vue')
+      },
+      {
+        hidden: true,
+        path: '/accountDetail',
+        name: 'accountDetail',
+        component: () => import('@/views/admin/AccountDetail.vue')
+      }
+    ]
   },
   {
-    hidden: true,
-    path: '/accountDetail',
-    name: 'accountDetail',
-    component: () => import('@/views/admin/AccountDetail.vue')
+    path: '/person',
+    name: 'Person',
+    redirect: '/personList',
+    meta: { title: '人员管理', icon: 'table' },
+    component: Layout,
+    children: [
+      {
+        path: '/personList',
+        name: 'personList',
+        meta: { title: '人员列表', icon: 'table' },
+        component: () => import('@/views/admin/PersonList.vue')
+      },
+      {
+        hidden: true,
+        path: '/personDetail',
+        name: 'personDetail',
+        component: () => import('@/views/admin/PersonDetail.vue')
+      }
+    ]
   },
   {
-    path: '/personList',
-    name: 'personList',
-    meta: { title: '人员列表', icon: 'table' },
-    component: () => import('@/views/admin/PersonList.vue')
+    path: '/role',
+    name: 'Role',
+    redirect: '/roleList',
+    component: Layout,
+    children: [
+      {
+        path: '/roleList',
+        name: 'roleList',
+        meta: { title: '角色列表', icon: 'table' },
+        component: () => import('@/views/admin/RoleList.vue')
+      },
+      {
+        hidden: true,
+        path: '/roleDetail',
+        name: 'roleDetail',
+        component: () => import('@/views/admin/RoleDetail.vue')
+      },
+      {
+        hidden: true,
+        path: '/rolePermissionDetail',
+        name: 'rolePermissionDetail',
+        component: () => import('@/views/admin/RolePermissionDetail.vue')
+      },
+      {
+        hidden: true,
+        path: '/roleAccountDetail',
+        name: 'roleAccountDetail',
+        component: () => import('@/views/admin/RoleAccountDetail.vue')
+      }
+    ]
   },
   {
-    hidden: true,
-    path: '/personDetail',
-    name: 'personDetail',
-    component: () => import('@/views/admin/PersonDetail.vue')
-  },
-  {
-    path: '/roleList',
-    name: 'roleList',
-    meta: { title: '角色列表', icon: 'table' },
-    component: () => import('@/views/admin/RoleList.vue')
-  },
-  {
-    hidden: true,
-    path: '/roleDetail',
-    name: 'roleDetail',
-    component: () => import('@/views/admin/RoleDetail.vue')
-  },
-  {
-    hidden: true,
-    path: '/roleAccountDetail',
-    name: 'roleAccountDetail',
-    component: () => import('@/views/admin/RoleAccountDetail.vue')
-  },
-  {
-    hidden: true,
-    path: '/rolePermissionDetail',
-    name: 'rolePermissionDetail',
-    component: () => import('@/views/admin/RolePermissionDetail.vue')
-  },
-  {
-    hidden: true,
-    path: '/dataCatalogDetail',
-    name: 'dataCatalogDetail',
-    component: () => import('@/views/admin/DataCatalogDetail.vue')
-  },
-  {
-    path: '/dataCatalogList',
-    name: 'dataCatalogList',
-    meta: { title: '数据字典列表', icon: 'table' },
-    component: () => import('@/views/admin/DataCatalogList.vue')
+    path: '/dataCatalog',
+    name: 'DataCatalogList',
+    redirect: '/dataCatalogList',
+    component: Layout,
+    children: [
+      {
+        hidden: true,
+        path: '/dataCatalogDetail',
+        name: 'dataCatalogDetail',
+        component: () => import('@/views/admin/DataCatalogDetail.vue')
+      },
+      {
+        path: '/dataCatalogList',
+        name: 'dataCatalogList',
+        meta: { title: '数据字典列表', icon: 'table' },
+        component: () => import('@/views/admin/DataCatalogList.vue')
+      }
+    ]
   },
   {
     path: '/permission',
@@ -116,22 +149,30 @@ export const adminRouter = [
     ]
   },
   {
-    path: '/orgList',
-    name: 'orgList',
-    meta: { title: '单位列表', icon: 'table' },
-    component: () => import('@/views/admin/OrgList.vue')
-  },
-  {
-    hidden: true,
-    path: '/orgDetail',
-    name: 'orgDetail',
-    component: () => import('@/views/admin/OrgDetail.vue')
-  },
-  {
-    hidden: true,
-    path: '/orgPersonDetail',
-    name: 'orgPersonDetail',
-    component: () => import('@/views/admin/OrgPersonDetail'.vue)
+    path: '/org',
+    name: 'Org',
+    redirect: '/orgList',
+    component: Layout,
+    children: [
+      {
+        path: '/orgList',
+        name: 'orgList',
+        meta: { title: '单位列表', icon: 'table' },
+        component: () => import('@/views/admin/OrgList.vue')
+      },
+      {
+        hidden: true,
+        path: '/orgDetail',
+        name: 'orgDetail',
+        component: () => import('@/views/admin/OrgDetail.vue')
+      },
+      {
+        hidden: true,
+        path: '/orgPersonDetail',
+        name: 'orgPersonDetail',
+        component: () => import('@/views/admin/OrgPersonDetail.vue')
+      }
+    ]
   },
   {
     path: '/person',
