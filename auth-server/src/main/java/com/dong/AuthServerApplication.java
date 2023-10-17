b5d876d33362367c2c3fe87af213411aa44ac332
liudong <liudong@zsoft.com.cn>
2023-10-17 15:27:55 +0800

    commit

--- auth-server/src/main/java/com/dong/AuthServerApplication.java
+++ auth-server/src/main/java/com/dong/AuthServerApplication.java
@@ -3,9 +3,6 @@ package com.dong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@ComponentScan(basePackages = {"com.dong.**", "com.dong.security.config"})
-//@EnableJpaRepositories(basePackages = {"com.dong.security.core.dao","com.dong.authserver.web.service"})
-//@EntityScan(basePackages = {"com.dong.security.core.entity"})
 @SpringBootApplication
 public class AuthServerApplication {
 
