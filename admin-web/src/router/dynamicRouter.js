bad2a1cb6526e8dd86519b6c42200e7ff00fdc72
liudong <15170519059@163.com>
2024-06-05 18:04:53 +0800

    commit

---
+++ admin-web/src/router/dynamicRouter.js
@@ -0,0 +1,33 @@
+import { findRouteList } from '@/api/menu'
+import Layout from '@/layout/index.vue'
+import router from '@/router/index'
+
+// 更新路由
+export async function fetchRouter() {
+  const response = await findRouteList() // 调用API获取路由
+  const routes = response.data // 假设路由数据在响应的data字段中
+  addRoutes(routes) // 添加路由
+}
+
+// 添加路由
+function addRoutes(routes) {
+  routes.forEach(route => {
+    if (route.component) {
+      if (route.component === 'Layout') {
+        route.component = Layout
+      } else {
+        const path = route.component
+        route.component = loadView(path)
+      }
+    }
+    if (route.children && route.children.length) {
+      addRoutes(route.children) // 递归处理子路由
+    }
+    router.addRoute(route) // 动态添加路由
+  })
+}
+
+// 加载页面
+function loadView(view) {
+  return () => import(`@/views/${view}.vue`)
+}
--- admin-web/src/router/index.js
+++ admin-web/src/router/index.js
@@ -106,34 +106,4 @@ export function resetRouter() {
   router.matcher = newRouter.matcher
 }
 
-// 更新路由
-export async function fetchRouter() {
-  const response = await findRouteList() // 调用API获取路由
-  const routes = response.data // 假设路由数据在响应的data字段中
-  addRoutes(routes) // 添加路由
-}
-
-// 添加路由
-function addRoutes(routes) {
-  routes.forEach(route => {
-    if (route.component) {
-      if (route.component === 'Layout') {
-        route.component = Layout
-      } else {
-        const path = route.component
-        route.component = loadView(path)
-      }
-    }
-    if (route.children && route.children.length) {
-      addRoutes(route.children) // 递归处理子路由
-    }
-    router.addRoute(route) // 动态添加路由
-  })
-}
-
-// 加载页面
-function loadView(view) {
-  return () => import(`@/views/${view}.vue`)
-}
-
 export default router
