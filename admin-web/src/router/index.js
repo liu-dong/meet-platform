8835ca03188a03f7224bfbc32cc7a1a2cf225d88
liudong <964708273@qq.com>
2024-06-15 18:24:18 +0800

    commit

--- admin-web/src/router/index.js
+++ admin-web/src/router/index.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from '@/layout'
-// import AdminRouter from '@/router/adminRouter'
+import NProgress from 'nprogress'
+import getPageTitle from '@/utils/get-page-title'
+import { getToken } from '@/utils/auth'
+import store from '@/store'
+import { fetchRoutes } from '@/router/dynamicRoute'
+import { checkRoutePermission, otherRoutes } from '@/router/route'
+import { Message } from 'element-ui'
 
 Vue.use(Router)
 
@@ -43,7 +49,71 @@ const createRouter = () => new Router({
   routes: constantRoutes
 })
 
+// 创建路由
 const router = createRouter()
+
+// 白名单
+const whiteList = ['/login', '/portal']
+
+// 进度条配置
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
+
+// 前置路由
+router.beforeEach(async(to, from, next) => {
+  // 开始进度条
+  NProgress.start()
+  document.title = getPageTitle(to.meta.title)
+  const hasToken = getToken()
+  if (hasToken) {
+    if (to.path === '/login') {
+      // 如果已经登录，跳转到首页
+      next({ path: '/home' })
+      NProgress.done()
+    } else {
+      const hasGetUserInfo = store.getters.nickname
+      if (!hasGetUserInfo) {
+        try {
+          // get user info
+          await store.dispatch('user/getInfo')
+          // 基于用户角色，动态获取并加载路由
+          const accessedRoutes = await fetchRoutes()
+          accessedRoutes.push(...otherRoutes)
+          // 存储路由
+          store.dispatch('GenerateRoutes', accessedRoutes).then(() => {
+            // 动态添加可访问路由表
+            router.addRoutes(accessedRoutes)
+            next({ ...to, replace: true })
+          })
+          // 添加侧边栏路由
+          await store.dispatch('SetSidebarRouters', accessedRoutes)
+          // hack方法，确保addRoutes已完成
+          next({ ...to, replace: true })
+        } catch (error) {
+          // 移除token,重新登录
+          await store.dispatch('user/resetToken')
+          Message.error(error || '遇到错误，请重新登录')
+          next(`/login?redirect=${to.path}`)
+          NProgress.done()
+        }
+      }
+      await checkRoutePermission(to, next)
+    }
+  } else {
+    /* has no token*/
+    if (whiteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      next(`/login`)
+      NProgress.done()
+    }
+  }
+})
+
+// 后置路由
+router.afterEach(() => {
+  // 结束进度条
+  NProgress.done()
+})
 
 // 重置路由
 export function resetRouter() {
---
+++ admin-web/src/router/route.js
@@ -0,0 +1,95 @@
+import Layout from '@/layout'
+import checkRole from '@/utils/permission'
+import adminRoute from '@/router/adminRoute'
+
+/**
+ * constantRoutes
+ * 一个不需要权限要求的基本页面
+ * 所有角色都可以访问
+ */
+export const constantRoute = [
+  {
+    path: '/portal',
+    component: () => import('@/views/portal/index'),
+    hidden: true
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        name: 'Home',
+        component: () => import('@/views/home/index'),
+        meta: { title: '首页', icon: 'dashboard' }
+      }
+    ]
+  },
+  ...adminRoute,
+  ...otherRoutes
+
+]
+
+// 其他路由
+export const otherRoutes = [
+  {
+    path: '/plan',
+    component: Layout,
+    redirect: '/plan/list',
+    children: [
+      {
+        path: '/list',
+        component: () => import('@/views/plan/list'),
+        name: 'Plan',
+        meta: { title: '计划制定', icon: 'table', roles: ['SuperAdmin'] }
+      }
+    ]
+  },
+  {
+    path: 'external-link',
+    component: Layout,
+    children: [
+      {
+        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        meta: { title: 'External Link', icon: 'link' }
+      }
+    ]
+  },
+  {
+    path: '/401',
+    component: () => import('@/views/error-page/401'),
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/error-page/404'),
+    hidden: true
+  },
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+/**
+ * 校验路由权限
+ * @param to
+ * @param next
+ * @returns {Promise<void>}
+ */
+export function checkRoutePermission(to, next) {
+  if (to.meta && to.meta.roles) {
+    const checkResult = checkRole(to.meta.roles)
+    if (checkResult) {
+      next()
+    } else {
+      next({ path: '/401' })
+    }
+  } else {
+    next()
+  }
+}
+
